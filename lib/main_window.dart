11e15001c16e87c152f6552669a1ac7eca8eb9b6
Michu_Linux <michalch001@gmail.com>
2023-12-11 16:49:31 +0100

    pierwszy

---
+++ lib/main_window.dart
@@ -0,0 +1,13 @@
+class Main_window extends StatefulWidget {
+  const Main_window({super.key});
+
+  @override
+  State<Main_window> createState() => _Main_windowState();
+}
+
+class _Main_windowState extends State<Main_window> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
